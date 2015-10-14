69c7551268851ad364337855bd8ea67ca59a49bb
Clement Lu <lu.705@osu.edu>
2015-10-14 12:17:27 -0400

    Changed title

--- app/src/main/java/mobiledev/group2/com/gallery/MainActivity.java
+++ app/src/main/java/mobiledev/group2/com/gallery/MainActivity.java
@@ -58,13 +58,13 @@ public class MainActivity extends AppCompatActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                //mTitle = "Gallery: " + getString(R.string.title_section1);
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+                //mTitle = "Gallery: " + getString(R.string.title_section2);
                 break;
             case 3:
-                mTitle = getString(R.string.title_section3);
+                //mTitle = "Gallery: " + getString(R.string.title_section3);
                 break;
         }
     }
